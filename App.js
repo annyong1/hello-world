06a9565c71fd41a16c028cf5e1e343c42e38a0d3
annyong1 <fbcjeremy@gmail.com>
2024-11-19 20:19:21 -0500

    5.1.2

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View, Text, TextInput, Button, Alert, ScrollView } from 'react-native';
 
 const App = () => {
+  const [text, setText] = useState('');
+  const alertMyText = () => {
+    Alert.alert(text);
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.blue}>Hello World! </Text>
-      <Text style={styles.bigRed}>How are you? </Text>
-      <Text style={[styles.blue, styles.bigRed]}>I'm feeling blue! </Text>
-      <View style={styles.box}></View>
+      <TextInput
+        style={styles.textInput}
+        value={text}
+        onChangeText={setText}
+        placeholder='Type Something Here'
+      />
+      <Text style={styles.textDisplay}>
+        You wrote: {text}  
+      </Text>
+      <Button
+        onPress={() => {
+          alertMyText();
+        }}
+        title="Press Me"
+      />
+      <ScrollView>
+        <Text style={{fontSize:170}}>This text is so big! And so long! You have to scroll!</Text>
+      </ScrollView>
     </View>
   );
 }
@@ -15,22 +34,18 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
   },
-  blue: {
-    color: 'blue',
-    fontWeight: '600',
+  textInput: {
+    width: '88%',
+    borderWidth: 1,
+    height: 50,
+    padding: 10
   },
-  bigRed: {
-    color: 'red',
-    fontSize: 30,
-  },
-  box: {
-    width: 60,
-    height: 60,
-    backgroundColor: 'blue',
+  textDisplay: {
+    height: 50,
+    lineHeight: 50
   }
 });
 
